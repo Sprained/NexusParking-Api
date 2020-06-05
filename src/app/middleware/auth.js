b472e74c55e4455b3b98d38ef7f3f9661558af9e
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2020-06-05 16:11:49 -0300

    middleware de auth

---
+++ src/app/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const util = require('util');
+const { promisify } = util;
+
+const authConfig = require('../../config/auth');
+
+module.exports = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).json('Token não informado!');
+    }
+
+    const [, token] = authHeader.split(' ');
+
+    try {
+        const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+        req.userId = decoded.id;
+
+        return next();
+    } catch (error) {
+        return res.status(401).json('Token invalido');
+    }
+}
